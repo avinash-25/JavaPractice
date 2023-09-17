c3b49d320dfa37e1a495ffb0a2a47acdfc02bb92
avinash-25 <avinashranjan268@gmail.com>
2023-09-17 17:29:06 +0530

    Z

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,14 +1,28 @@
-//public class BinarySearch {
-//    public static void main(String[] args) {
-//
-//    }
-//    static int binarySearch(int[] arr, int target){
-//        int start =0;
-//        int end = arr.length-1;
-//
-//        while(start <= end){
-//            int mid = start +  (start+end)/2;
-//
-//        }
-//    }
-//}
+class BinarySearch {
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == target) {
+                return mid; // Found the target element
+            } else if (arr[mid] < target) {
+                start = mid + 1; // Target is in the right half
+            } else {
+                end = mid - 1; // Target is in the left half
+            }
+        }
+
+        return -1; // Target not found in the array
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,4,5,6,7,89};
+        int val = 50;
+        int num = binarySearch(arr,val);
+        System.out.println("Returned value is : "+num);
+    }
+}
---
+++ OrderAgnosticBS.java
@@ -0,0 +1,4 @@
+//static int OrderAgnosticBS(int[] arr,int target) {
+//    int start=0;
+//    int end = arr.length-1;
+//}
